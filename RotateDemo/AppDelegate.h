85c80cdcca9af710b0139dd1b925e6beb09fa31b
HaiLee <comcuter@gmail.com>
2013-05-05 14:36:02 +0800

    Initial Commit

---
+++ RotateDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RotateDemo
+//
+//  Created by HaiLee on 13-5-5.
+//  Copyright (c) 2013年 Hai Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ RotateDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RotateDemo
+//
+//  Created by HaiLee on 13-5-5.
+//  Copyright (c) 2013年 Hai Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
